2ce049ac180721141f510a5fb1443107db6e1474
NihalSWE <mahamudur35-2563@diu.edu.bd>
2023-06-26 02:41:05 +0600

    First Commit

---
+++ 1st.py/add_two_numbers.py
@@ -0,0 +1,8 @@
+def add_two_numbers(first,second):
+    return first+second
+X=int(input("ENter the First Number:"))
+Y=int(input("ENter the Second Number:"))
+
+z=add_two_numbers(X,Y)
+print(f"Your Answer is : {z}")
+print("Thank You")
